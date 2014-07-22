2b73b91d793bcbd14499f863286236ca69d83bbd
blueplanet <blueplanet42@gmail.com>
2014-07-23 05:39:06 +0900

    init commit

---
+++ src/com/voyager/MyServlet.java
@@ -0,0 +1,47 @@
+package com.voyager;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String str = request.getParameter("text1");
+        request.setAttribute("msg", str);
+        // リダイレクト
+        ServletContext context = this.getServletContext();
+        RequestDispatcher dispatcher = context.getRequestDispatcher("/page.jsp");
+        dispatcher.forward(request, response);
+	}
+
+}
